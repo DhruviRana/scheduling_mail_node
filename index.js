41d3c18850235acb3b1f8918e6e01b2ee9705b18
DhruviRana <104005702+DhruviRana@users.noreply.github.com>
2022-12-07 15:29:52 +0530

    ok

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+// const { response } = require('express');
 const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
-
-
-
-
-
-
-var server = app.listen(3000, () => {
+const { response } = require('express');
+const port = process.env.PORT || 3000;
+var server = app.listen(port, () => {
     console.log("start");
 })
 app.get('/sendmail', (req, res) => {
@@ -47,6 +44,8 @@ app.get('/sendmail', (req, res) => {
     }
     var cron2 = "" + response1.crons + "";
     console.log(cron2);
+    var cron3 = "*/10 * * * * *";
+    console.log(cron3);
     cron.schedule(cron2, function() {
         console.log("running a task every 10 second");
         sendMail();
